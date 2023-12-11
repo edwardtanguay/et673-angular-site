07f5011b51a776ccdfae20fa14f8e23a36fcd4f1
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 14:23:30 +0100

    ngModel

--- src/app/page-employees/page-employees.component.ts
+++ src/app/page-employees/page-employees.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { IEmployee } from '../../shared/interfaces';
 import { CommonModule } from '@angular/common';
 import axios from 'axios';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-page-employees',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule],
 	templateUrl: './page-employees.component.html',
 	styleUrl: './page-employees.component.scss',
 })
 export class PageEmployeesComponent {
 	employees: IEmployee[] = [];
+	searchText: string = '';
 
 	constructor() {
 		(async () => {
