0d91e9b960c43565412e5de3b00aa394545aa38a
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 10:55:45 +0100

    basic info-box created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { InfoBoxComponent } from "./info-box/info-box.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, InfoBoxComponent]
 })
 export class AppComponent {
   title = 'The Info Site';
