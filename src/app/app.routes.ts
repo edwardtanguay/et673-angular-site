41aad5c405b84bdb5b0eb81651d443b148c5a7b3
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 13:17:15 +0100

    simplified menu, three pages work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PageHomeComponent } from './page-home/page-home.component';
 import { PageInfoComponent } from './page-info/page-info.component';
+import { PageEmployeesComponent } from './page-employees/page-employees.component';
 
 export const routes: Routes = [
 	{ path: 'home', component: PageHomeComponent},
-	{ path: 'info', component: PageInfoComponent}
+	{ path: 'info', component: PageInfoComponent},
+	{ path: 'employees', component: PageEmployeesComponent}
 ];
