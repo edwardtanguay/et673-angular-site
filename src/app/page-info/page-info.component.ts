b3984d17bf0b9beb41dbac0f4b69aa65481e0ec8
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 13:19:50 +0100

    three pages work

--- src/app/page-info/page-info.component.ts
+++ src/app/page-info/page-info.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { InfoBoxComponent } from "../info-box/info-box.component";
 
 @Component({
-  selector: 'app-page-info',
-  standalone: true,
-  imports: [],
-  templateUrl: './page-info.component.html',
-  styleUrl: './page-info.component.scss'
+    selector: 'app-page-info',
+    standalone: true,
+    templateUrl: './page-info.component.html',
+    styleUrl: './page-info.component.scss',
+    imports: [InfoBoxComponent]
 })
 export class PageInfoComponent {
 
