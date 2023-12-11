f14126e6be5e32d411a144573dcd886d647d91c5
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 14:12:41 +0100

    mock employees in

--- src/app/page-employees/page-employees.component.ts
+++ src/app/page-employees/page-employees.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { IEmployee } from '../../shared/interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-page-employees',
-  standalone: true,
-  imports: [],
-  templateUrl: './page-employees.component.html',
-  styleUrl: './page-employees.component.scss'
+	selector: 'app-page-employees',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './page-employees.component.html',
+	styleUrl: './page-employees.component.scss',
 })
 export class PageEmployeesComponent {
+	employees: IEmployee[] = [];
 
+	constructor() {
+		this.employees = [
+			{
+				firstName: 'Frank',
+				lastName: 'Holander',
+				notes: "These are Frank's notes.",
+			},
+			{
+				firstName: 'Selma',
+				lastName: 'Schmidt',
+				notes: "These are Selma's notes.",
+			},
+			{
+				firstName: 'Angie',
+				lastName: 'Ramadesh',
+				notes: "These are Angie's notes.",
+			},
+		];
+	}
 }
