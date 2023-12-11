46d51db3f4654abbdd608034a19ad6561697dc86
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 11:48:20 +0100

    dynamic highlight works

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -13,10 +13,12 @@ export class InfoBoxComponent {
 	@Input() body = '(body)';
 	@Input() author = '(author)';
 	@Input() status = '(status unknown)';
+	@Input() highlight = '';
 
 	dateOfInfo = 'Dec 23, 2022';
 	statusColor = '';
 	showDevInfo = false;
+	highlighted = true;
 
 	ngOnInit() {
 		this.statusColor = this.status === 'success' ? 'lightgreen' : this.status === 'danger' ? 'tomato' : 'gray';
