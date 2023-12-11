3d0d2a334b73a73896e60521976571e6a0d10ae8
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 11:56:21 +0100

    base top-menu in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { InfoBoxComponent } from "./info-box/info-box.component";
+import { TopMenuComponent } from "./top-menu/top-menu.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, InfoBoxComponent]
+    imports: [CommonModule, RouterOutlet, InfoBoxComponent, TopMenuComponent]
 })
 export class AppComponent {
   title = 'The Info Site';
-	message = 'This is a test.';
 }
