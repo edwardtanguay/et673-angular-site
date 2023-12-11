ac8fd5ef279027e4e6ab4a3ddad1c09d9a54c4be
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 11:23:08 +0100

    ngStyle works

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -1,15 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-info-box',
-  standalone: true,
-  imports: [],
-  templateUrl: './info-box.component.html',
-  styleUrl: './info-box.component.scss'
+	selector: 'app-info-box',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './info-box.component.html',
+	styleUrl: './info-box.component.scss',
 })
 export class InfoBoxComponent {
 	@Input() title = '(title)';
 	@Input() body = '(body)';
 	@Input() author = '(author)';
+	@Input() status = '';
+
 	dateOfInfo = 'Dec 23, 2022';
+	statusColor = '';
+
+	ngOnInit() {
+		this.statusColor = this.status === 'success' ? 'lightgreen' : 'red';
+	}
 }
