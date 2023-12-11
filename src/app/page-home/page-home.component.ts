8fe8d3040a9e4e0ce1bea50698099e9d2e1eab5a
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 13:46:06 +0100

    all built-in pipes

--- src/app/page-home/page-home.component.ts
+++ src/app/page-home/page-home.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { Component } from '@angular/core';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 @Component({
-  selector: 'app-page-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './page-home.component.html',
-  styleUrl: './page-home.component.scss'
+	selector: 'app-page-home',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './page-home.component.html',
+	styleUrl: './page-home.component.scss',
 })
 export class PageHomeComponent {
-
+	now = '2023-12-23 13:21:01';
 }
