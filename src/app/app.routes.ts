18e6484286d022b8a71e135bf99384290da530e9
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 13:27:57 +0100

    menu works also with 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { PageHomeComponent } from './page-home/page-home.component';
 import { PageInfoComponent } from './page-info/page-info.component';
 import { PageEmployeesComponent } from './page-employees/page-employees.component';
+import { Page404Component } from './page-404/page-404.component';
 
 export const routes: Routes = [
-	{ path: 'home', component: PageHomeComponent},
-	{ path: 'info', component: PageInfoComponent},
-	{ path: 'employees', component: PageEmployeesComponent}
+	{
+		path: '',
+		redirectTo: '/home',
+		pathMatch: 'full',
+	},
+	{ path: 'home', component: PageHomeComponent },
+	{ path: 'info', component: PageInfoComponent },
+	{ path: 'employees', component: PageEmployeesComponent },
+	{ path: '**', component: Page404Component },
 ];
